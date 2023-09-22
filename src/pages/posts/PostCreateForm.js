1f523f41852d2e5134daf7093a38db54432f12e7
Matt Manning <mtmanning93@hotmail.co.uk>
2023-09-22 15:14:26 +0000

    Create onChange attribute with handleChange function to update the state of create form

--- src/pages/posts/PostCreateForm.js
+++ src/pages/posts/PostCreateForm.js
@@ -11,18 +11,38 @@ import Upload from "../../assets/upload.png";
 import styles from "../../styles/PostCreateEditForm.module.css";
 import appStyles from "../../App.module.css";
 import btnStyles from "../../styles/Button.module.css";
+import Asset from "../../components/Asset";
 
 function PostCreateForm() {
+  const [postData, setPostData] = useState({
+    title: "",
+    content: "",
+    image: "",
+  });
+
+  const { title, content, image } = postData;
 
   const [errors, setErrors] = useState({});
 
+  const handleChange = (event) => {
+    setPostData({
+      ...postData,
+      [event.target.name]: event.target.value,
+    });
+  };
 
   const textFields = (
     <div className="text-center">
-      {/* Add your form fields here */}
+      <Form.Group>
+        <Form.Label>Title</Form.Label>
+        <Form.Control onChange={handleChange} value={title} type="text" name="title" />
+      </Form.Group>
 
-    
-    
+      <Form.Group>
+        <Form.Label>Content</Form.Label>
+        <Form.Control onChange={handleChange} value={content} as="textarea" rows={6} name="content" />
+      </Form.Group>
+
       <Button
         className={`${btnStyles.Button} ${btnStyles.Blue}`}
         onClick={() => {}}
@@ -43,14 +63,12 @@ function PostCreateForm() {
             className={`${appStyles.Content} ${styles.Container} d-flex flex-column justify-content-center`}
           >
             <Form.Group className="text-center">
-              
-                <Form.Label
-                  className="d-flex justify-content-center"
-                  htmlFor="image-upload"
-                >
-                  ASSET
-                </Form.Label>
-
+              <Form.Label
+                className="d-flex justify-content-center"
+                htmlFor="image-upload"
+              >
+                <Asset src={Upload} message="Click or tap to upload an image" />
+              </Form.Label>
             </Form.Group>
             <div className="d-md-none">{textFields}</div>
           </Container>
