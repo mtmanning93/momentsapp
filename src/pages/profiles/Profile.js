18b09430cb3c90bf48b1eaf2b8b00f249ec42d8a
Matt Manning <mtmanning93@hotmail.co.uk>
2023-09-25 12:50:26 +0000

    Create profile styles with name and avatar plus the follow and unfollow buttons

---
+++ src/pages/profiles/Profile.js
@@ -0,0 +1,52 @@
+import React from "react";
+import styles from "../../styles/Profile.module.css";
+import btnStyles from "../../styles/Button.module.css";
+import { useCurrentUser } from "../../context/CurrentUserContext";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import Avatar from "../../components/Avatar";
+import { Button } from "react-bootstrap";
+
+const Profile = (props) => {
+  const { profile, mobile, imageSize = 55 } = props;
+  const { id, following_id, image, owner } = profile;
+
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
+
+  return (
+    <div
+      className={`my-3 d-flex align-items-center ${mobile && "flex-column"}`}
+    >
+      <div>
+        <Link className="align-self-center" to={`/projects/${id}`}>
+          <Avatar src={image} height={imageSize} />
+        </Link>
+        <div className={`mx-2 ${styles.WordBreak}`}>
+          <strong>{owner}</strong>
+        </div>
+      </div>
+      <div className={`text-rigth ${!mobile && "ml-auto"}`}>
+        {!mobile &&
+          currentUser &&
+          !is_owner &&
+          (following_id ? (
+            <Button
+              className={`${btnStyles.Button} ${btnStyles.BlackOutline}`}
+              onClick={() => {}}
+            >
+              unfollow
+            </Button>
+          ) : (
+            <Button
+              className={`${btnStyles.Button} ${btnStyles.Black}`}
+              onClick={() => {}}
+            >
+              following
+            </Button>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
