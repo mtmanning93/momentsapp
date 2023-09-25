49515d6cff8a5630ab200b26732911bc3982caee
Matt Manning <mtmanning93@hotmail.co.uk>
2023-09-25 09:55:48 +0000

    Add comment css

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -22,10 +22,12 @@ function PostPage() {
   useEffect(() => {
     const handleMount = async () => {
       try {
-        const [{ data: post }] = await Promise.all([
+        const [{ data: post }, { data: comments }] = await Promise.all([
           axiosReq.get(`/posts/${id}`),
+          axiosReq.get(`/comments/?post=${id}`),
         ]);
         setPost({ results: [post] });
+        setComments(comments);
         console.log(post);
       } catch (err) {
         console.log(err);
@@ -52,6 +54,17 @@ function PostPage() {
           ) : comments.results.length ? (
             "Comments"
           ) : null}
+          {comments.results.length ? (
+            comments.results.map((comment) => (
+              <p key={comment.id}>
+                {comment.owner}: {comment.content}
+              </p>
+            ))
+          ) : currentUser ? (
+            <span>No comments yet, be the first to comment!</span>
+          ) : (
+            <span>No comments...yet!</span>
+          )}
         </Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
