e1073ec16306aa4f3aaa0215b63539156f571336
Matt Manning <mtmanning93@hotmail.co.uk>
2023-09-25 09:19:06 +0000

    Add delete image functionality

--- src/components/MoreDropdown.js
+++ src/components/MoreDropdown.js
@@ -15,7 +15,7 @@ const ThreeDots = React.forwardRef(({ onClick }, ref) => (
   />
 ));
 
-export const MoreDropdown = () => {
+export const MoreDropdown = ({handleEdit, handleDelete}) => {
   return (
     <Dropdown className="ml-auto" drop="left">
       <Dropdown.Toggle as={ThreeDots} />
@@ -26,14 +26,14 @@ export const MoreDropdown = () => {
       >
         <Dropdown.Item
           className={styles.DropdownItem}
-          onClick={() => {}}
+          onClick={handleEdit}
           aria-label="edit"
         >
           <i className="fas fa-edit" />
         </Dropdown.Item>
         <Dropdown.Item
           className={styles.DropdownItem}
-          onClick={() => {}}
+          onClick={handleDelete}
           aria-label="delete"
         >
           <i className="fas fa-trash-alt" />
