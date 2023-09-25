2726a00132fe3363ba9eb81ca941d9397c8a2623
Matt Manning <mtmanning93@hotmail.co.uk>
2023-09-25 11:30:59 +0000

    Fix bug with comment deletion in api and add infinite scroll functionailtiy to comments

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -11,6 +11,9 @@ import Post from "./Post";
 import CommentCreateForm from "../comments/CommentCreateFrom";
 import { useCurrentUser } from "../../context/CurrentUserContext";
 import Comment from "../comments/Comment";
+import InfiniteScroll from "react-infinite-scroll-component";
+import Asset from "../../components/Asset";
+import { fetchMoreData } from "../../utils/utils";
 
 function PostPage() {
   const { id } = useParams();
@@ -55,14 +58,23 @@ function PostPage() {
             "Comments"
           ) : null}
           {comments.results.length ? (
-            comments.results.map((comment) => (
-              <Comment
-                key={comment.id}
-                {...comment}
-                setPost={setPost}
-                setComments={setComments}
-              />
-            ))
+            <InfiniteScroll
+              children={
+                comments.results.map((comment) => (
+                  <Comment
+                    key={comment.id}
+                    {...comment}
+                    setPost={setPost}
+                    setComments={setComments}
+                  />
+                ))
+              }
+              dataLength={comments.results.length}
+              loader={<Asset spinner />}
+              hasMore={!!comments.next}
+              next={() => fetchMoreData(comments, setComments)}
+            >
+            </InfiniteScroll>
           ) : currentUser ? (
             <span>No comments yet, be the first to comment!</span>
           ) : (
